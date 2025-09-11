c5a9c31008364e88c21a2e27b96e581ca4816afc
Enzo Bustamante <enzobustamante895@gmail.com>
2025-09-11 19:01:25 -0300

    update: baseUrl with school ipv4

--- app/src/main/java/com/example/offerkotlin/data/remote/RetroFitInstance.kt
+++ app/src/main/java/com/example/offerkotlin/data/remote/RetroFitInstance.kt
@@ -7,7 +7,7 @@ object RetroFitInstance {
     val api: OfferApi by lazy {
         Retrofit.Builder()
            // .baseUrl("http://10.0.2.2:8080/api/") // ⚠️ emulator → localhost
-            .baseUrl("http://192.168.1.16:8080/api/") // ifconfig linux, ipconfig windows
+            .baseUrl("http://10.0.14.3:8080/api/") // ifconfig o "hostname -I" linux, ipconfig windows
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(OfferApi::class.java)
